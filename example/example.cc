05827734a753e5a93f3d1eed460d4b51f7bd3fda
Alexander Hurd <hurdad@gmail.com>
2018-10-07 18:45:13 -0400

    example update

--- example/example.cc
+++ example/example.cc
@@ -11,38 +11,54 @@
 int main() {
 
   // spectral periodogram options
-  unsigned int nfft        =  	 1<<17;  // spectral periodogram FFT size
-  unsigned int num_samples =      10e6;  // number of samples
-  unsigned int i;
-  float psd[nfft];
+  unsigned int nfft        =  	  1<<15;  // spectral periodogram FFT size
+  unsigned int num_samples =      100e6;  // number of samples
+  float psd[nfft];                        // output
 
-  // generate signal
-  std::cout << "generating input signal size : " << num_samples << std::endl;
-  // generate signal
+  // generate QPSK signal
+  std::cout << "generating QPSK signal size : " << num_samples << std::endl;
   std::vector<std::complex<float>> *y = new std::vector<std::complex<float>>(num_samples);
-  for (i = 0; i < num_samples; i++) {
-    std::complex<float> s = 0;
-    double theta = (double) i / (double) num_samples * M_PI;
-    s.real(1.0 * cos(10.0 * theta) + 0.5 * cos(25.0 * theta));
-    s.imag(1.0 * cos(10.0 * theta) + 0.5 * cos(25.0 * theta));
 
-    // save
-    (*y)[i] = s;
-  }
+  // create stream generator
+  msourcecf gen = msourcecf_create();
 
-  // start timer for cuda
-  std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+  // add noise source (narrow-band)
+  int id_noise = msourcecf_add_noise(gen, 0.10f);
+  msourcecf_set_frequency(gen, id_noise, 0.4*2*M_PI);
+  msourcecf_set_gain     (gen, id_noise, -20.0f);
+
+  // add tone
+  int id_tone = msourcecf_add_tone(gen);
+  msourcecf_set_frequency(gen, id_tone, -0.4*2*M_PI);
+  msourcecf_set_gain     (gen, id_tone, -10.0f);
+
+  // add modulated data
+  int id_modem = msourcecf_add_modem(gen,LIQUID_MODEM_QPSK,4,12,0.30f);
+  msourcecf_set_frequency(gen, id_modem, -0.1*2*M_PI);
+  msourcecf_set_gain     (gen, id_modem, 0.0f);
+
+  // generate samples into y
+  msourcecf_write_samples(gen, y->data(), num_samples);
 
   // create cuda spectral periodogram
   //CudaSpGramCF* q = CudaSpGramCF::create_default(nfft);
   CudaSpGramCF* q = CudaSpGramCF::create(nfft, LIQUID_WINDOW_BLACKMANHARRIS7, nfft, nfft);
   q->print();
-  q->write(y->data(), num_samples);
-  q->get_psd(psd);
-  q->export_gnuplot(CUDA_OUTPUT_FILENAME);
 
-  //  print duration
-  std::cout << "cuda-spgram duration ms: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - t1).count() << std::endl;
+  // start timer for cuda
+  std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+
+  //  write IQ data to periodgram
+  q->write(y->data(), num_samples);
+
+  //  populate output
+  q->get_psd(psd);
+ 
+  //  print write duration
+  std::cout << "cuda-spgram write duration ms: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - t1).count() << std::endl;
+
+  // export plot
+  q->export_gnuplot(CUDA_OUTPUT_FILENAME);
 
   //  print statistics
   printf("cuda spgram stats:\n");
@@ -51,18 +67,25 @@ int main() {
   printf("total_num_transforms : %" PRIu64 "\n", q->get_num_transforms());
   printf("total_num_transforms_total : %" PRIu64 "\n", q->get_num_transforms_total());
 
-  // start timer liquid
-  std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-
+  // create liquid spectral periodogram
   //spgramcf qq = spgramcf_create_default(nfft);
   spgramcf qq = spgramcf_create(nfft, LIQUID_WINDOW_BLACKMANHARRIS7, nfft, nfft);
   spgramcf_print(qq);
-  spgramcf_write(qq, y->data(), num_samples);
-  spgramcf_get_psd(qq, psd);
-  spgramcf_export_gnuplot(qq, LIQUID_OUTPUT_FILENAME);
 
-  //  print duration
-  std::cout << "liquid spgram duration ms: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - t2).count() << std::endl;
+  // start timer liquid
+  std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+  
+  //  write IQ data to periodgram
+  spgramcf_write(qq, y->data(), num_samples);
+
+  //  populate output
+  spgramcf_get_psd(qq, psd);
+
+  //  print write duration
+  std::cout << "liquid spgram write duration ms: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - t2).count() << std::endl;
+
+  // export plot
+  spgramcf_export_gnuplot(qq, LIQUID_OUTPUT_FILENAME);
 
   //  print statistics
   printf("liquid spgram stats:\n");
@@ -71,7 +94,7 @@ int main() {
   printf("total_num_transforms : %" PRIu64 "\n", spgramcf_get_num_transforms(qq));
   printf("total_num_transforms_total : %" PRIu64 "\n", spgramcf_get_num_transforms_total(qq));
 
-  // destroy object
+  // destroy objecta
   delete(y);
   delete(q);
   spgramcf_destroy(qq);
